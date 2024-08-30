46f2a3eb4214069ab162188e201de5d4c1b97b41
ttcmd-main <118116436+ttcmd-main@users.noreply.github.com>
2024-08-30 05:28:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+tasks_amount = 12 # Количество выполненных ДЗ
+hours_spent = 1.5 # Количество затраченных часов
+course = 'Python' # Название курса
+time_per_exercise = hours_spent/tasks_amount # Время на одно задание
+print('Курс:',course,', всего задач:',tasks_amount,',','затрачено часов:',hours_spent, 'среднее время выполнения',time_per_exercise   )
